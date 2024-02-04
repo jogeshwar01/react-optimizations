04216e6e456b08eef4df89d387bfa29abccb1213
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2024-02-04 20:44:03 +0530

    useCallback and useRef examples

---
+++ frontend/src/hooks/2-use-callback/Assignment1.tsx
@@ -0,0 +1,31 @@
+import { memo, useCallback, useState } from "react";
+
+// Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
+
+export function Assignment1() {
+    const [count, setCount] = useState(0);
+
+    const handleIncrement = useCallback(() => {
+        setCount(count => count + 1)
+        // will need to add count dependency if we use the syntax - setCount(count + 1)
+    },[])
+    
+
+    const handleDecrement = useCallback(() => {
+        setCount(count => count - 1)
+    },[])
+
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <CounterButtons onIncrement={handleIncrement} onDecrement={handleDecrement} />
+        </div>
+    );
+};
+
+const CounterButtons = memo({ onIncrement, onDecrement }) => (
+    <div>
+        <button onClick={onIncrement}>Increment</button>
+        <button onClick={onDecrement}>Decrement</button>
+    </div>
+);
